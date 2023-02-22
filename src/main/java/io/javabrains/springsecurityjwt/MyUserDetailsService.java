1d1296bdb64b28c49c82baef2b3b9d9e07bfbf6d
jmatharu2 <reck24386@yahoo.com>
2023-02-22 12:16:13 -0500

    added missing code

--- src/main/java/io/javabrains/springsecurityjwt/MyUserDetailsService.java
+++ src/main/java/io/javabrains/springsecurityjwt/MyUserDetailsService.java
@@ -1,24 +1,40 @@
 package io.javabrains.springsecurityjwt;
 
-import org.springframework.security.core.userdetails.User;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import io.javabrains.springsecurityjwt.models.MyUserDetails;
+import io.javabrains.springsecurityjwt.repo.UserRepository;
+import io.javabrains.springsecurityjwt.repo.User;
+
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
-    @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        /*return new User("foo", "foo",
-                new ArrayList<>());*/       
-        return  User.builder()
-        		.username("foo")				
-                .password("foo")
-                .roles("USER")
-                .build();
-    }
+	/*
+	 * @Override public UserDetails loadUserByUsername(String s) throws
+	 * UsernameNotFoundException { return new User("foo", "foo", new ArrayList<>());
+	 * return User.builder() .username("foo") .password("foo") .roles("USER")
+	 * .build();
+	 * 
+	 * }
+	 */
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		Optional<User> user = userRepository.findByUserName(userName);
+
+		user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+		return user.map(MyUserDetails::new).get();
+	}
+
 }
